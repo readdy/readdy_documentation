650b5c428a3302628856375c8ca18c0068f79d8c
chrisfroe <christoph.froehner@fu-berlin.de>
2017-12-15 17:22:36 +0100

    highlighting of search results

--- readdy_documentation/assets/js/search.js
+++ readdy_documentation/assets/js/search.js
@@ -1,58 +1,73 @@
 // adapted from http://jekyll.tips/jekyll-casts/jekyll-search-using-lunr-js/
-(function() {
-  function displaySearchResults(results, store) {
-    var searchResults = document.getElementById('search-results');
+(function () {
+    function displaySearchResults(searchTerm, results, store) {
+        var searchResults = document.getElementById('search-results');
 
-    if (results.length) { // Are there any results?
-      var appendString = '';
+        if (results.length) { // Are there any results?
+            var appendString = '';
 
-      for (var i = 0; i < results.length; ++i) {  // Iterate over the results
-        var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
-      }
+            for (var i = 0; i < results.length; ++i) {  // Iterate over the results
+                var item = store[results[i].ref];
+                appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+                // find first occurence with str.search(), get substring around that and highlight
+                var termBegin = item.content.search(searchTerm);
+                var termEnd = termBegin + searchTerm.length;
+                var displayBegin = termBegin - 75;
+                var displayEnd = termBegin + 75;
+                if (displayBegin < 0) {
+                    displayEnd += -1 * displayBegin;
+                    displayBegin = 0;
+                }
+                appendString += '<p>'
+                    + item.content.substring(displayBegin, termBegin)
+                    + "<span style='background-color: #fff085'>"
+                    + item.content.substring(termBegin, termEnd)
+                    + "</span>"
+                    + item.content.substring(termEnd, displayEnd)
+                    + '...</p></li>';
+            }
 
-      searchResults.innerHTML = appendString;
-    } else {
-      searchResults.innerHTML = '<li>No results found</li>';
+            searchResults.innerHTML = appendString;
+        } else {
+            searchResults.innerHTML = '<li>No results found</li>';
+        }
     }
-  }
 
-  function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
+    function getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
 
-    for (var i = 0; i < vars.length; ++i) {
-      var pair = vars[i].split('=');
+        for (var i = 0; i < vars.length; ++i) {
+            var pair = vars[i].split('=');
 
-      if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-      }
+            if (pair[0] === variable) {
+                return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+            }
+        }
     }
-  }
 
-  var searchTerm = getQueryVariable('query');
+    var searchTerm = getQueryVariable('query');
 
-  if (searchTerm) {
-    document.getElementById('search-box').setAttribute("value", searchTerm);
+    if (searchTerm) {
+        document.getElementById('search-box').setAttribute("value", searchTerm);
 
-    // Initalize lunr with the fields it will be searching on. I've given title
-    // a boost of 10 to indicate matches on this field are more important.
-    var idx = lunr(function () {
-      this.field('id');
-      this.field('title', { boost: 10 });
-      this.field('content');
-    });
+        // Initalize lunr with the fields it will be searching on. I've given title
+        // a boost of 10 to indicate matches on this field are more important.
+        var idx = lunr(function () {
+            this.field('id');
+            this.field('title', {boost: 10});
+            this.field('content');
+        });
 
-    for (var key in window.store) { // Add the data to lunr
-      idx.add({
-        'id': key,
-        'title': window.store[key].title,
-        'content': window.store[key].content
-      });
+        for (var key in window.store) { // Add the data to lunr
+            idx.add({
+                'id': key,
+                'title': window.store[key].title,
+                'content': window.store[key].content
+            });
 
-      var results = idx.search(searchTerm); // Get lunr to perform a search
-      displaySearchResults(results, window.store);
+            var results = idx.search(searchTerm); // Get lunr to perform a search
+            displaySearchResults(searchTerm, results, window.store);
+        }
     }
-  }
 })();
