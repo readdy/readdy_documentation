ade467c6d8f7c4cb9db54208cd908f75f234c6d5
chrisfroe <christoph.froehner@fu-berlin.de>
2017-12-15 10:49:30 +0100

    restore native scrollbar for content container, sidebar container still has javascript scrollbar

--- readdy_documentation/assets/js/scrollbar.js
+++ readdy_documentation/assets/js/scrollbar.js
@@ -1,4 +1,2 @@
 const sideContainer = document.getElementById('unique-side-container');
 Ps.initialize(sideContainer);
-const mainContainer = document.getElementById('unique-main-container');
-Ps.initialize(mainContainer);
