4d26f1485267918d8a006bd2b1943fe0f8b8719c
clonker <clonker@gmail.com>
2018-01-08 14:47:53 +0100

    updated jekyll version

--- readdy_documentation/_serve.sh
+++ readdy_documentation/_serve.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bundle exec jekyll serve -d $(pwd)/build --trace --config _config.yml,_config_dev.yml --host 0.0.0.0
+bundle exec jekyll serve -d $(pwd)/build --trace --config _config.yml,_config_dev.yml --host localhost
